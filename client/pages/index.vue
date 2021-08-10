388ebd3d43a98724c2e247f8878b48ce22005061
Sitt Aung <sittaung2021@gmail.com>
2021-08-10 16:09:11 +0630

    (client) base styles with tailwind

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
-    Home
+    <h1 class="font-medium text-4xl">Snippets</h1>
+    <p>A little more text in here</p>
   </div>
 </template>
 
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        header: ["Rubik", "sans-serif"]
+      }
+    }
   },
   variants: {},
   plugins: []
