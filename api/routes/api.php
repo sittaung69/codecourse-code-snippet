70eb3505b916a53e9ae363145ada48aad1145633
Sitt Aung <sittaung2021@gmail.com>
2021-08-11 10:25:48 +0630

    (api) getting authenticated user information

--- api/routes/api.php
+++ api/routes/api.php
@@ -4,4 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'auth', 'namespace' => 'App\Http\Controllers\Auth'], function () {
     Route::post('signin', 'SignInController');
+    Route::get('me', 'MeController');
 });
